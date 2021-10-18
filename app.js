4528d3d534e51f9001b6991ff305f770f131ffa6
netomachado <netomachado72@gmail.com>
2021-10-18 15:05:53 -0300

    adding cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const session = require('express-session');
+const cors = require('cors');
 
 
 const indexRouter = require('./routes/index');
@@ -16,6 +17,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
